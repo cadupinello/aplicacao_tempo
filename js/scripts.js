d15208b768068f37ef95b1cd841f9c1bf9243d83
cadupinello <carloseduardo_0900@outlook.com>
2022-10-10 13:31:17 -0300

    Ajuda do instrutor mattheus Batista hora de codar.

---
+++ js/scripts.js
@@ -0,0 +1,67 @@
+const apiKey = "23dea7b3a57fef0228ab3503ff88d1b4";
+const apiCountryURL = "https://countryflagsapi.com/png/"
+
+const cityInput = document.querySelector("#city-input")
+const searchBtn = document.querySelector("#search")
+
+const cityElement =document.querySelector("#city")
+const tempElement =document.querySelector("#temperature span")
+const descElement =document.querySelector("#description")
+const weatherIconElement =document.querySelector("#weather-icon")
+const countryElement =document.querySelector("#country")
+const humidityElement =document.querySelector("#humidity span")
+const windElement =document.querySelector("#wind span")
+
+const weatherContainer = document.querySelector("#weather-data")
+const loader = document.querySelector("#loader")
+
+// Loader
+const toggleLoader = () => {
+  loader.classList.toggle("hide")
+}
+
+// Funções
+const getWeatherData = async (city) => {
+  toggleLoader()
+
+  const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
+  const res = await fetch(apiWeatherURL)
+  const data = await res.json()
+
+  toggleLoader()
+
+  return data
+}
+
+const showWeatherData = async (city) => {
+  const data = await getWeatherData(city)
+
+  cityElement.innerText = data.name
+  tempElement.innerText = parseInt(data.main.temp)
+  descElement.innerText = data.weather[0].description
+  weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country)
+  humidityElement.innerText = `${data.main.humidity}%`
+  windElement.innerText = `${data.wind.speed}km/h`
+
+  weatherContainer.classList.remove("hide")
+
+}
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  const city = cityInput.value
+
+  showWeatherData(city)
+})
+
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value
+
+    showWeatherData(city)
+  }
+})
